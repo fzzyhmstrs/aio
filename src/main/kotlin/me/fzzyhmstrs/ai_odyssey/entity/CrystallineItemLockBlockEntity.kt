2bf907a866b6799aaffe48743d3d8de8af11500e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-05-31 21:05:45 -0400

    block entities for the door locks

---
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/entity/CrystallineItemLockBlockEntity.kt
@@ -0,0 +1,11 @@
+package me.fzzyhmstrs.ai_odyssey.entity
+
+import me.fzzyhmstrs.ai_odyssey.registry.RegisterEntity
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.util.math.BlockPos
+
+class CrystallineItemLockBlockEntity(pos: BlockPos, state: BlockState):BlockEntity(RegisterEntity.CRYSTALLINE_ITEM_LOCK_BLOCK_ENTITY,pos, state) {
+
+
+}
--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/registry/RegisterEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/registry/RegisterEntity.kt
@@ -1,8 +1,9 @@
 package me.fzzyhmstrs.ai_odyssey.registry
 
 import me.fzzyhmstrs.ai_odyssey.AIO
+import me.fzzyhmstrs.ai_odyssey.entity.CrystallineItemLockBlockEntity
+import me.fzzyhmstrs.ai_odyssey.entity.CrystallineNumLockBlockEntity
 import me.fzzyhmstrs.ai_odyssey.entity.CrystallineSwitchBlockEntity
-import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterBlock
 import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntityType
@@ -19,7 +20,27 @@ object RegisterEntity {
                 pos,
                 state
             )
-        }, RegisterBlock.FORCEFIELD_BLOCK).build(null))
+        }, RegisterBlock.CRYSTALLINE_SWITCH).build(null))
+
+    val CRYSTALLINE_NUM_LOCK_BLOCK_ENTITY: BlockEntityType<CrystallineNumLockBlockEntity> = Registry.register(
+        Registry.BLOCK_ENTITY_TYPE,
+        AIO.MOD_ID + ":crystalline_switch_entity",
+        FabricBlockEntityTypeBuilder.create({ pos: BlockPos, state: BlockState ->
+            CrystallineNumLockBlockEntity(
+                pos,
+                state
+            )
+        }, RegisterBlock.CRYSTALLINE_NUM_LOCK).build(null))
+
+    val CRYSTALLINE_ITEM_LOCK_BLOCK_ENTITY: BlockEntityType<CrystallineItemLockBlockEntity> = Registry.register(
+        Registry.BLOCK_ENTITY_TYPE,
+        AIO.MOD_ID + ":crystalline_switch_entity",
+        FabricBlockEntityTypeBuilder.create({ pos: BlockPos, state: BlockState ->
+            CrystallineItemLockBlockEntity(
+                pos,
+                state
+            )
+        }, RegisterBlock.CRYSTALLINE_ITEM_LOCK).build(null))
 
     fun registerAll(){
 
