d3890468c5a3e0fd66e10a25df2d3d5777027885
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-10 20:30:10 -0400

    work on aberration spawn block

--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/block/AberrationSpawnBlock.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/block/AberrationSpawnBlock.kt
@@ -1,14 +1,37 @@
 package me.fzzyhmstrs.ai_odyssey.block
 
+import me.fzzyhmstrs.ai_odyssey.entity.AberrationSpawnBlockEntity
+import me.fzzyhmstrs.ai_odyssey.entity.CrystallineSwitchBlockEntity
+import me.fzzyhmstrs.ai_odyssey.registry.RegisterEntity
 import net.minecraft.block.BlockState
 import net.minecraft.block.BlockWithEntity
 import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class AberrationSpawnBlock(settings: Settings): BlockWithEntity(settings) {
 
-    override fun createBlockEntity(pos: BlockPos?, state: BlockState?): BlockEntity? {
-        TODO("Not yet implemented")
+    override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
+        return AberrationSpawnBlockEntity(pos, state)
+    }
+
+    override fun <T : BlockEntity> getTicker(
+        world: World,
+        state: BlockState,
+        type: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        return if(!world.isClient) checkType(
+            type, RegisterEntity.ABERRATION_SPAWN_BLOCK_ENTITY
+        ) { world2: World, pos: BlockPos, state2: BlockState, blockEntity: AberrationSpawnBlockEntity ->
+            AberrationSpawnBlockEntity.tick(
+                world2,
+                pos,
+                state2,
+                blockEntity
+            )
+        } else null
     }
 
 }
--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/entity/AberrationSpawnBlockEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/entity/AberrationSpawnBlockEntity.kt
@@ -4,6 +4,17 @@ import me.fzzyhmstrs.ai_odyssey.registry.RegisterEntity
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class AberrationSpawnBlockEntity(pos: BlockPos, state: BlockState):BlockEntity(RegisterEntity.CRYSTALLINE_NUM_LOCK_BLOCK_ENTITY,pos, state) {
+
+
+    companion object {
+        fun tick(world: World, pos: BlockPos, state: BlockState, blockEntity: AberrationSpawnBlockEntity) {
+
+        }
+
+    }
+
+
 }
