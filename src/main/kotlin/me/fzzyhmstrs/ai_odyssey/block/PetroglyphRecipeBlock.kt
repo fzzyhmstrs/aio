a50bec9ad7ecceb0e7901a91c498dc651b6e3dc3
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-06-21 11:57:10 -0400

    Update PetroglyphRecipeBlock.kt

--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/block/PetroglyphRecipeBlock.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/block/PetroglyphRecipeBlock.kt
@@ -6,19 +6,55 @@ import net.minecraft.state.StateManager
 import net.minecraft.state.property.EnumProperty
 import net.minecraft.util.StringIdentifiable
 
-class PetroglyphRecipeBlock(settings: Settings): Block(settings) {
+class PetroglyphRecipeBlock(settings: Settings): Block(settings), ConfiguratorInteractive {
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
-        builder.add(INGREDIENT)
+        builder.add(FACING,INGREDIENT)
+    }
+    
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
+        return super.getPlacementState(ctx)?.with(INGREDIENT,Ingredient.NONE)?.with(FACING,ctx.playerFacing.opposite)
+    }
+    
+    override fun interactWithConfigurator(
+        world: World, 
+        user: PlayerEntity?, 
+        stack: ItemStack, 
+        pos: BlockPos, 
+        state: BlockState): ActionResult{
+        return if (user != null) {
+            user.openHandledScreen(state.createScreenHandlerFactory(world, pos))
+            ActionResult.CONSUME
+        } else {
+            ActionResult.FAIL        
+        }
+    }
+    
+    @Deprecated("Deprecated in Java")
+    override fun createScreenHandlerFactory(
+        state: BlockState,
+        world: World,
+        pos: BlockPos
+    ): NamedScreenHandlerFactory {
+        val text = this.name
+        return SimpleNamedScreenHandlerFactory({ syncId: Int, inventory: PlayerInventory, _: PlayerEntity ->
+            PetroglyphRecipeScreenHandler(
+                syncId,
+                inventory,
+                ScreenHandlerContext.create(world, pos)
+            )
+        }, text)
     }
 
     companion object {
+        private val FACING = HorizontalFacingBlock.FACING
         private val INGREDIENT: EnumProperty<Ingredient> = EnumProperty.of("ingredient", Ingredient::class.java)
 
         enum class Ingredient: StringIdentifiable{
 
             DIAMOND,
-            EMERALD;
+            EMERALD,
+            NONE;
 
             override fun asString(): String {
                 return this.name
