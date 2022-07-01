ab6e4dfa49eb288e7d51491dd41c92fca989165e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-01 10:29:44 -0400

    tweaks to mystic armor material

--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/armor/MysticArmorMaterial.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/armor/MysticArmorMaterial.kt
@@ -7,6 +7,7 @@ import net.minecraft.recipe.Ingredient
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 
+@Suppress("PrivatePropertyName")
 class MysticArmorMaterial: ArmorMaterial {
     private val BASE_DURABILITY = intArrayOf(13, 15, 16, 11)
     private val PROTECTION_VALUES = intArrayOf(5, 9, 10, 5)
@@ -16,7 +17,7 @@ class MysticArmorMaterial: ArmorMaterial {
     override fun getRepairIngredient(): Ingredient = Ingredient.ofItems(RegisterItem.MYSTIC_QUINTESSENCE)
     override fun getEnchantability(): Int = 25
     override fun getProtectionAmount(slot: EquipmentSlot): Int = PROTECTION_VALUES[slot.entitySlotId]
-    override fun getDurability(slot: EquipmentSlot): Int = PROTECTION_VALUES[slot.entitySlotId] * 62
+    override fun getDurability(slot: EquipmentSlot): Int = BASE_DURABILITY[slot.entitySlotId] * 62
     override fun getKnockbackResistance(): Float = 0.15F
     override fun getToughness(): Float = 4.0F
 }
