224d637981c0e965b7674ad71787e5f9bcd89c4f
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-17 21:01:24 -0400

    gargantuan kelp buildout continues

--- src/main/kotlin/me/fzzyhmstrs/ai_odyssey/registry/RegisterTag.kt
+++ src/main/kotlin/me/fzzyhmstrs/ai_odyssey/registry/RegisterTag.kt
@@ -1,6 +1,9 @@
 package me.fzzyhmstrs.ai_odyssey.registry
 
 import me.fzzyhmstrs.ai_odyssey.AIO
+import me.fzzyhmstrs.amethyst_imbuement.AI
+import net.minecraft.block.Block
+import net.minecraft.item.Item
 import net.minecraft.tag.TagKey
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -11,4 +14,8 @@ object RegisterTag {
     val PORTAL_FACILITY_TAG: TagKey<ConfiguredStructureFeature<*, *>> = TagKey.of(Registry.CONFIGURED_STRUCTURE_FEATURE_KEY,
         Identifier(AIO.MOD_ID,"portal_facility")
     )
+    val GARGANTUAN_KELP_LOGS_TAG: TagKey<Block> = TagKey.of(Registry.BLOCK_KEY,Identifier(AIO.MOD_ID,"gargantuan_kelp_logs"))
+    val GARGANTUAN_SKELETAL_KELP_LOGS_TAG: TagKey<Block> = TagKey.of(Registry.BLOCK_KEY,Identifier(AIO.MOD_ID,"gargantuan_skeletal_kelp_logs"))
+    val GARGANTUAN_KELP_LOGS_ITEM_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AIO.MOD_ID,"gargantuan_kelp_logs"))
+    val GARGANTUAN_SKELETAL_KELP_LOGS_ITEM_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AIO.MOD_ID,"gargantuan_skeletal_kelp_logs"))
 }
